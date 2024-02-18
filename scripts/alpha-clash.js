fa5d265b1b693ae302ac5fff867ba7d10bdc078d
jaheda8256 <jahedasultana47@gmail.com>
2024-02-19 00:25:25 +0600

    added hidded section

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,25 @@
+function play(){
+    
+    const ticketingSection = document.getElementById('ticketing-interface');
+    ticketingSection.classList.add('hidden');
+
+
+    const ticketingSection2 = document.getElementById('bus-info');
+    ticketingSection2.classList.add('hidden');
+
+
+    const ticketingSection3 = document.getElementById('card-section');
+    ticketingSection3.classList.add('hidden');
+
+
+    const ticketingSection4 = document.getElementById('banner-section');
+    ticketingSection4.classList.add('hidden');
+
+
+    const ticketingSection5 = document.getElementById('navbar');
+    ticketingSection5.classList.add('hidden');
+
+
+    const successSection = document.getElementById('success');
+    successSection.classList.remove('hidden');
+}
