740cdd5bb2e49d50678c80a93e87fdd3b352b1ba
jaheda8256 <jahedasultana47@gmail.com>
2024-02-19 17:13:49 +0600

    added ticket section

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -6,44 +6,54 @@ for(const btn of allBtn){
     btn.addEventListener('click', function(e){
         count = count + 1;
      
-
-        const ticketName = e.target.innerText;
-        const ticketName2 = "Economoy";
-        const ticketName3 = "550";
         const selectedContainer = document.getElementById('selected-ticket');
+        const ticketSeat = e.target.innerText;
+        const ticketClass = "Economoy";
+        const ticketPrice = "550";
+        
 
+       
+
+        // table create
        const tr = document.createElement('tr');
        const td = document.createElement('td');
-       td.innerText = ticketName;
+       td.innerText = ticketSeat;
        const td2 = document.createElement('td');
-       td2.innerText = ticketName2;
+       td2.innerText = ticketClass;
        const td3 = document.createElement('td');
-       td3.innerText = ticketName3;
-
-       e.target.style.backgroundColor = '#1DD100FF';
+       td3.innerText = ticketPrice;
 
        tr.appendChild(td);
        tr.appendChild(td2);
        tr.appendChild(td3);
-       selectedContainer.appendChild(tr);
+     
 
        const score = document.getElementById('score').innerText;
        const convertedScore = parseInt(score);
 
         if(convertedScore - parseInt(1) < 36){
-            alert('Your ticket is full').to;
-            return;
-
-            
+            alert('Your ticket is full');
+            return;   
         }
-
-
-   document.getElementById('score').innerText = convertedScore - parseInt(1);
-    totalCost('total-price', parseInt(ticketName3))
-    grandTotal('grand-total', parseInt(ticketName3));  
-    setInnerText('btn-count',count);
+        selectedContainer.appendChild(tr);
+        e.target.style.backgroundColor = '#1DD100FF';
      
-    })
+// total discount Amount 
+
+
+if(totalCost >= 2200 && couponCode === "NEW15"){
+    const discountElement = document.getElementById('grand-total');
+    const discountAmount = totalCost * 0.85;
+  discountElement.innerText = discountAmount.toFixed(2);
+    
+}
+
+document.getElementById('score').innerText = convertedScore - parseInt(1);
+totalCost('total-price', parseInt(ticketPrice))
+grandTotal('grand-total', parseInt(ticketPrice));  
+setInnerText('btn-count',count);
+ 
+})
 
 }
 
